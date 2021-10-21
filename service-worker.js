e2614675eee74b888c3cbfd5e78ebd05c82a27b9
miltoskr <miltoskartalas@gmail.com>
2021-10-21 18:33:27 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.165cbf55f3c4e9b1f410adb982f76f22.js"
+  "/precache-manifest.6b522104eb918aaa6d4574c4a27881fc.js"
 );
 
 self.addEventListener('message', (event) => {
